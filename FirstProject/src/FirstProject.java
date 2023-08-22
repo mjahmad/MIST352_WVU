308d7ac6a012f120ac63da01fc72d3e40942d854
mahmad2 <mahmad2@BNE-BXSFTQ3.wvu-ad.wvu.edu>
2023-08-22 09:57:46 -0400

    Added my newest project

---
+++ FirstProject/src/FirstProject.java
@@ -0,0 +1,25 @@
+/**Author: MJ Ahmad
+ * Date: 8/22/2023 
+ */
+
+/**
+ * 
+ */
+public class FirstProject {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my first project");
+		int intNumber1 = 0;
+		int intNumber2=10;
+		System.out.println("Number 1 is: "+(intNumber1));
+		System.out.println("Number 2 is: "+(intNumber2));
+		System.out.println("Number 2 is: "+(intNumber2));
+
+
+	}
+
+}
