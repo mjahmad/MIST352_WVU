a9c7c4f592b5ebdfe0dc71eaf1fc64c2248ff2c5
mjahmad <mahmad2@mix.wvu.edu>
2023-09-12 10:35:38 -0400

    modified Equations again

--- In-Class-Activities/Equations/src/Equations.java
+++ In-Class-Activities/Equations/src/Equations.java
@@ -1,9 +1,14 @@
-
+/*
+ * Program to test some equations
+ * Chapter 2 9/12/23
+ */
 public class Equations {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		
+		//Just some equations to test
 		int intResult1 = -14 + 6 * 9 % 10 * 10;
 		
 		int intResult2 = 9 * 6 % 10 / 3 - 1 + 100;
@@ -13,6 +18,11 @@ public class Equations {
 		System.out.println(intResult1);
 		System.out.println(intResult2);
 		System.out.println(intResult3);
+		
+		double intMathTester = Math.sqrt(10);
+		System.out.println(intMathTester);
+
+				
 
 
 	}
