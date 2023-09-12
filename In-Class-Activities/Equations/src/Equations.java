0e5a8bdb0709fa495b9d0c62943cf7dc626a9705
mjahmad <mahmad2@mix.wvu.edu>
2023-09-12 10:32:47 -0400

    modified Equations

--- In-Class-Activities/Equations/src/Equations.java
+++ In-Class-Activities/Equations/src/Equations.java
@@ -3,7 +3,17 @@ public class Equations {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("ee");
+		
+		int intResult1 = -14 + 6 * 9 % 10 * 10;
+		
+		int intResult2 = 9 * 6 % 10 / 3 - 1 + 100;
+
+		int intResult3 = 100 - 9 % 3 * 61 / 10 ;
+		
+		System.out.println(intResult1);
+		System.out.println(intResult2);
+		System.out.println(intResult3);
+
 
 	}
 
