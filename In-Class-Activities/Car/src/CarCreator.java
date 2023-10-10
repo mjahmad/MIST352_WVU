1b81cac7236e90a5df44d35f649a4c12a294a781
MJ <mahmad2@mix.wvu.edu>
2023-10-10 09:05:47 -0400

    Added Class Car

---
+++ In-Class-Activities/Car/src/CarCreator.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author MJ
+ *
+ */
+public class CarCreator {
+
+	   public static void main(String[] args) {
+	        // Create a Car object
+	        Car myCar = new Car("Toyota", "Camry", 2020, 25000.0);
+
+	        // Display car information
+	        myCar.displayInfo();
+	    }
+
+}
