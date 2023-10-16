673f3a93c914cec9fd177d1b9770b6edc62ad36e
MJ <mahmad2@mix.wvu.edu>
2023-10-16 14:02:41 -0400

    added extra class to MyPayRoll

---
+++ In-Class-Activities/MyPayroll/src/PayrollMain.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class PayrollMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		Payroll employee1 = new Payroll("MJ",100);
+//
+//		Payroll employee2 = new Payroll("Kait",101);
+//
+//		Payroll employee3 = new Payroll("John",102);
+//
+//		Payroll employee4 = new Payroll("Sarah",103);
+//
+//		Payroll employee5 = new Payroll("Hilary");
+//		
+//		Payroll employee = new Payroll();
+//		employee.setName("John");
+//		System.out.println(employee.getName());
+//		String strTEmpEmployee = employee.getName();
+//		employee.setID(100);
+//		employee.setPayRate(0.9);
+//		employee.setWorkedHours(100);
+//		employee.printEmplolyeeData();
+//		employee.PrintSalary();
+//		
+//		
+//		Payroll employee1 = new Payroll();
+//		employee1.setName("Hilary");
+//		employee1.setID(200);
+//		employee1.setPayRate(0.9);
+//		employee1.setWorkedHours(200);
+//		employee1.printEmplolyeeData();
+//		employee1.PrintSalary();
+//		
+//		double milk = employee.salary();
+//		
+//	
+		Course course1 = new Course("MIS352");
+		
+		Course course2 = new Course("MIS353",30);
+
+		Course course3 = new Course(30,0);
+		
+		Course course4 = new Course();
+
+		
+	}
+
+}
