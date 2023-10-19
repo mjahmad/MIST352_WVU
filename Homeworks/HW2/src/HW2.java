48573b43ea0754092fafaf8b25e1dad7fead9e87
MJ <mahmad2@mix.wvu.edu>
2023-10-18 23:36:09 -0400

    Added homework2

---
+++ Homeworks/HW2/src/HW2.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+
+/**
+ * @author MJ
+ *
+ */
+public class HW2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Course course1 = new Course("MIST352", "Businsess Application Programming","MJ Ahmad");
+		Course course2 = new Course("CS101", "Introd. to Computer Science","Sarah Jones");
+		Student student1 = new Student("Ashley Jaber");
+		Student student2 = new Student("Ali June");
+		
+		//Keep If statements the way they are.
+		if (course1.enrollStudent()) 
+			{
+			
+			}
+
+		else
+			{
+			
+			}
+			
+		if (course2.enrollStudent())
+			{
+		
+			}
+
+		else
+			{
+			
+			}
+		
+		
+		
+		if (course1.enrollStudent())
+			{
+				
+			}
+
+		else
+			{
+				
+			}
+		
+		if (course2.enrollStudent()) 
+			{
+				
+			}
+		else
+			{
+				
+			}
+		//Add your code below for question 8
+		
+	}
+
+}
