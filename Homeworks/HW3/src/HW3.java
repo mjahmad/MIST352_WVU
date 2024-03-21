aceebd8786212c070653b9a650289a2a857cf537
MJ <mahmad2@mix.wvu.edu>
2024-03-21 00:02:10 -0400

    added HW3 starter

---
+++ Homeworks/HW3/src/HW3.java
@@ -0,0 +1,79 @@
+/**
+ * 
+ */
+
+/**
+ * @author MJ
+ *
+ */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+import javax.swing.JOptionPane;
+
+public class HW3 {
+
+	/**
+	 * @param args
+	 * @throws FileNotFoundException 
+	 */
+	@SuppressWarnings("resource")
+	public static void main(String[] args) throws FileNotFoundException {
+	//****** Keep as is *******************
+		// The location of the employee text file 
+		String strDataLocation ="src/EmployeeData.txt";
+		String strOutputLocation ="src/EmployeeSummary.txt";
+		File myFile = new File(strDataLocation);
+		Scanner inputFile = new Scanner (myFile);
+		PrintWriter writer =new PrintWriter(strOutputLocation);
+		int intChoice  = Integer.parseInt( JOptionPane.showInputDialog(null,  ViewOptions()));
+		DataSummary myData = new DataSummary(strDataLocation);
+	//*************************************
+
+		//This while loop controls the options for users. Your switch statement should be embedded here.
+		while(intChoice !=0)
+		{
+			switch(intChoice)
+			{
+			case 1:
+				myData.ViewEmployeeAndIDs();
+				break;
+			
+			}
+			intChoice  = Integer.parseInt( JOptionPane.showInputDialog(null,  ViewOptions()));	
+		}
+
+
+		writer.close();
+		inputFile.close();	
+		
+	}
+	/**	 
+	 * Keep as is
+	 * A method to display options
+	 * @return : The options to display in the GUI
+	 */
+	
+	public static String ViewOptions()
+	{
+		String strMessageToDisplay="Choose an option:"
+				+ "\n0.\tExit"
+				+ "\n1.\tView Employees and IDs"
+				+ "\n2.\tSearch for Employee"
+				+ "\n3.\tView current roles"
+				+ "\n4.\tView employees for a certain role"
+				+ "\n5.\tView Average Salary"
+				+ "\n6.\tSummerize and Write data";
+		return strMessageToDisplay;
+	} }
+	
+	
+	
+	
+
+
+
