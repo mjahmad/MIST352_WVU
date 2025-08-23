cc81c59af2eddf7a2cc5e0fc8d22726c00bfa3b6
MJ <mahmad2@mix.wvu.edu>
2025-08-22 21:02:50 -0400

    examples added

---
+++ In_Class_Examples/Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This is my first C# program");
+
+            // Lets define some variables
+
+            int intAge = 0;
+            double intPrice = 1.5;
+            String strName = " Mohammad J Ahmad";
+
+            //Printout variables
+            Console.WriteLine(strName);
+            Console.WriteLine(intPrice);
+            Console.WriteLine("Give me age");
+
+            //Read from usuer the age
+            intAge = Convert.ToInt32(Console.ReadLine());
+
+            //Print out age
+            Console.WriteLine(intAge);
+
+
+        }
+
+
+    }
+}
