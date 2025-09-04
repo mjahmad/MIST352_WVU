d28e12d08b717ce1773db6ba653b098b371e39fa
MJ Ahmad <mahmad2@mix.wvu.edu>
2025-09-04 09:57:47 -0400

    added practices we did in class this week

---
+++ In_Class_Examples/Practice2/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * MJ Ahmad
+ * Tuesday 9/2/25
+ * Practice trasnlating pseudo code to C#
+ * 
+ */
+
+namespace Practice2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* double dblVal1, dblVal2;
+             Console.WriteLine("Give me the first value: ");
+             dblVal1 = Convert.ToDouble(Console.ReadLine());
+             //test 
+             //Console.WriteLine(dblVal1);
+
+             Console.WriteLine("Give me the second value: ");
+             dblVal2 = Convert.ToDouble(Console.ReadLine());
+             //test 
+             double dblTotal = dblVal1 + dblVal2;
+             Console.WriteLine("The sum of {dblVal1} and {dblVal2} is {dblTotal}");*/
+
+            int dblVal1, dblVal2;
+            Console.WriteLine("Give me the first value: ");
+            dblVal1 = int.Parse(Console.ReadLine());
+            //test 
+            //Console.WriteLine(dblVal1);
+
+            Console.WriteLine("Give me the second value: ");
+            dblVal2 = int.Parse(Console.ReadLine());
+            //test 
+            double dblTotal = dblVal1 + dblVal2;
+            Console.WriteLine($"The sum of {dblVal1} and {dblVal2} is {dblTotal}");
+        }
+    }
+}
