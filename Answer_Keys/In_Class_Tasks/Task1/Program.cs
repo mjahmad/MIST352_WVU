7632ba2166954cf2842066400f37e622ea96cbcc
MJ Ahmad <mahmad2@mix.wvu.edu>
2025-09-05 00:10:18 -0400

    added answer key for Task1

---
+++ Answer_Keys/In_Class_Tasks/Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿// MJ Ahmad
+// Task1
+// Tuesday 8/26/25
+// This simple program simply prints out some message to the user
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+       
+            Console.WriteLine("====================================");
+            Console.WriteLine("Name\tAge\tAddress");
+            Console.WriteLine("MJ\t41\tMorgantown");
+            Console.WriteLine("Sarah\t22\tFairmont");
+            Console.WriteLine("Elizabeth\t30\tCharleston");
+            Console.WriteLine("====================================");
+
+
+
+
+
+        }
+    }
+
