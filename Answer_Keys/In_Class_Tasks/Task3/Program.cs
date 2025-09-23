853b74acdda51044e2b5f524963f1df1294e2fe8
MJ Ahmad <mahmad2@mix.wvu.edu>
2025-09-23 10:24:31 -0400

    adde while loops

---
+++ Answer_Keys/In_Class_Tasks/Task3/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+ */
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many itesm would yo like to order?>");
+            int intItemCount = int.Parse(Console.ReadLine());
+            
+            //define arrays of size intItemcount
+            string[] strNames = new string[intItemCount];
+            double[] dblPrices = new double[intItemCount];
+            int[] intQtys = new int[intItemCount];
+            int[] intStocks = new int[intItemCount];
+            double[] dblLineDiscounts = new double[intItemCount];
+            double[] dblLineTotals = new double[intItemCount];
+            bool[] blnReord = new bool[intItemCount];
+
+            //the main for loop to accept items data and store them in the arrays above.
+
+            for (int intIndex = 0; intIndex < strNames.Length; intIndex++)
+            { }
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ In_Class_Examples/Weeks 5,6, and 7/While_Loops/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Tuesday 9/23/25
+ * While loop and thier uses
+ */
+
+using System.Runtime.ExceptionServices;
+
+namespace While_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] dblNumbers = { 19.0, 18, 10, 0.7, 5.5 };
+            //for (int intIndex = 0; intIndex < dblNumbers.Length; intIndex++)
+            //{
+            //    Console.WriteLine(dblNumbers[intIndex]);
+            //}
+
+            //while loop
+            int intWhileIndex = 0;
+            while (intWhileIndex < dblNumbers.Length)
+            {
+                Console.WriteLine(dblNumbers[intWhileIndex]);
+                //intWhileIndex++;
+
+            }
+
+            //String strPassword = "";
+            //while (strPassword!="Cats")
+            //{
+            //    Console.WriteLine("Give me a password");
+            //    strPassword = Console.ReadLine();
+
+            //}
+
+
+
+        }
+    }
+}
