83afb0999d852eac828278c49164469a08d06913
MJ Ahmad <mahmad2@mix.wvu.edu>
2025-10-08 15:32:31 -0400

    added more to weeks 5-8 methods and references

---
+++ In_Class_Examples/Weeks 5,6,7, & 8/Methods_Arrays_Switch_3/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace Methods_Arrays_Switch_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strNames = { "Sarah","Ahmad","John", "Sarah","Tiffany", "Mike", "Rachel", "Sarah" , "Sarah" };
+            char chrUserChoice = 'A';
+            while (chrUserChoice != 'X')
+            {
+                WelcomeMessageAndOptions();
+                chrUserChoice = Console.ReadKey().KeyChar;
+
+                switch (chrUserChoice)
+                {
+                    case 'A':
+                        Console.WriteLine("\nOk this will printout the name in the array");
+                        PrintOutNames(strNames);
+                        break;
+                    case 'B':
+                        Console.WriteLine("\nOk this will search for a name in the array");
+                        Console.WriteLine("\nGive me a name to search for.");
+                        string strNameToSearchFor = Console.ReadLine();
+
+                        SearchForName(strNameToSearchFor, strNames);
+                        break;
+                    case 'X':
+                        Console.WriteLine("\nGood bye. The program will now exit");
+
+                        break;
+                    default:
+                        Console.WriteLine("\nInvalid inoput. Try again");
+                        break;
+
+                }
+
+
+            }
+
+
+
+          
+
+            
+        }
+
+        static void WelcomeMessageAndOptions()
+        {
+            Console.WriteLine("\nWelcome. Choose an option from below");
+            Console.WriteLine("\nA - printout all names in the array\nB- Search for a name in the array\nX- Exit hte program");
+        }
+
+        /// <summary>
+        /// Printout the contents of a given array
+        /// </summary>
+        /// <param name="strNames"> The array of data coming from outside</param>
+        static void PrintOutNames(string[] strNames)
+        {
+            for (int intIndex = 0; intIndex < strNames.Length; intIndex++)
+            {
+                Console.WriteLine(strNames[intIndex]);
+            }
+        }
+
+        /// <summary>
+        /// Finds and reports the locaiton of a given name in the given array of names
+        /// </summary>
+        /// <param name="strTheNameToFind"> The name to find in the array (external)</param>
+        /// <param name="strTheNamesArray"> The array of names to search in (external) </param>
+        static void SearchForName(string strTheNameToFind, string[] strTheNamesArray)
+        {
+            for (int intIndex = 0; intIndex < strTheNamesArray.Length; intIndex++)
+            {
+                if (strTheNamesArray[intIndex].Equals(strTheNameToFind))
+                {
+                    Console.WriteLine($"The name {strTheNameToFind} found in locantio {intIndex}");
+                }
+
+            }
+        }
+
+        
+    }
+}
---
+++ In_Class_Examples/Weeks 5,6,7, & 8/Methods_Passing_Types/Program.cs
@@ -0,0 +1,106 @@
+﻿namespace Methods_Passing_Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int intFirstVal = 1, intScondVal = 2, intThirdVal;
+
+            //Console.WriteLine($"Before method: {intFirstVal}\t {intScondVal}");
+            //Multiply(intFirstVal, intScondVal);
+            //Console.WriteLine(Multiply2(intFirstVal,intScondVal));
+            //MultiplyByReference(ref intFirstVal, ref intScondVal);
+            //Console.WriteLine($"after method: {intFirstVal}\t {intScondVal}");
+
+            //int[] intNumbers = { 10, 20, 30, 40 };
+            //PrintArray(intNumbers);
+            //Console.WriteLine(intNumbers[0]);
+            //Console.WriteLine($"Before ecalling the method {intFirstVal}\t{intScondVal}");
+
+            //SumValues(ref intFirstVal, ref intScondVal, out intThirdVal);
+            //Console.WriteLine($"After ecalling the method {intFirstVal}\t{intScondVal}\t{intThirdVal}");
+            String strUserEmail;
+            AcceptAndVerifyEmail(out strUserEmail);
+            Console.WriteLine(strUserEmail);
+
+
+
+        }
+
+
+        static void AcceptAndVerifyEmail(out String strTheEamil)
+        {
+
+            bool blnValidEmail = true;
+            strTheEamil = "";
+            while (blnValidEmail)
+
+            {
+                Console.WriteLine("What is your email?");
+                strTheEamil = Console.ReadLine();
+                if (strTheEamil.Contains("@") && strTheEamil.Contains("."))
+                {
+                    break;
+
+                }
+                else
+
+                {
+                    Console.WriteLine("Wrong email - provide it again");
+                    //strTheEamil = Console.ReadLine();
+
+
+                }
+
+
+
+            }
+
+
+
+        }
+
+        static void SumValues(ref int Val1, ref int Val2, out int Val3)
+        {
+            Val3 = 10;
+
+            Console.WriteLine(Val1 + Val2 + Val3);
+
+            Val1 = Val2 = Val3 = 0;
+
+        }
+
+
+        static void PrintArray(int[] intData)
+        {
+            intData[0] = 100;
+            for (int intIndex = 0; intIndex < intData.Length; intIndex++)
+                Console.WriteLine(intData[intIndex]);
+
+        }
+
+        static void MultiplyByReference(ref int Val1, ref int Val2)
+        {
+            Val1 = 20;
+            Console.WriteLine(Val1 * Val2);
+            Val2 = 90;
+
+        }
+
+        //static void Multiply(int Val1, int Val2)
+        //{
+        //    Val1 = 20;
+        //    Console.WriteLine(Val1 * Val2);
+        //}
+
+
+        //static int Multiply2(int Val1, int Val2)
+        //{
+        //    Val1 = 20;
+        //    return Val1 * Val2;
+        //}
+
+
+    }
+}
+
