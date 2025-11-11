380d226844489603264fb1b9162f71a0e6721581
MJ <mahmad2@mix.wvu.edu>
2025-11-10 19:05:12 -0500

    data

---
+++ Tasks/Task6/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+/*
+            Car BMW = new Car();
+            BMW.Model="";
+            BMW.DisplayInfo();
+
+            Car Malibuc = new Car("Chevy", "Malibu");
+            Malibuc.DisplayInfo();*/
+
+            Employee Kayla = new Employee("Kayla");
+            Kayla.DisplayInfo();
+            Kayla.HoursPerWeek = -1;
+            Kayla.HourlyRate = 101;
+            Kayla.DisplayInfo();
+
+
+
+        }
+    }
+}
