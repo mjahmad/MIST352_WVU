48d733506db9c0328204da3388b9e2cd1390bc80
MJ Ahmad <mahmad2@mix.wvu.edu>
2026-02-10 11:11:16 -0500

    adde Module 5 codes

---
+++ In_Class_Examples/Module 5/Operattors_If/Program.cs
@@ -0,0 +1,96 @@
+﻿namespace Operattors_If
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int intValue = 0;
+
+            //if (intValue > 0)
+            //{
+            //    Console.WriteLine($"{intValue} is positive");
+            //}
+            //else if (intValue < 0)
+            //{
+            //    Console.WriteLine($"{intValue} is negative");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{intValue} is zero, funny you!");
+
+            //}
+
+
+
+            ///////////////////////////////////////////////////
+            //double[] dblGrades = { 55,66,79,88,99};
+
+            //if (dblGrades[0] >= 60)
+            //{
+            //    Console.WriteLine($"{dblGrades[0]} => Valid and Pass");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{dblGrades[0]} => Fail");
+            //}
+
+            //if (dblGrades[1] >= 60)
+            //{
+            //    Console.WriteLine($"{dblGrades[1]} => Pass");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{dblGrades[1]} => Fail");
+            //}
+
+            //if (dblGrades[2] >= 60)
+            //{
+            //    Console.WriteLine($"{dblGrades[2]} => Pass");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{dblGrades[2]} => Fail");
+            //}
+
+            //if (dblGrades[3] >= 60)
+            //{
+            //    Console.WriteLine($"{dblGrades[3] }=> Pass");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{dblGrades[3]} => Fail");
+            //}
+
+            //if (dblGrades[4] >= 60)
+            //{
+            //    Console.WriteLine($"{dblGrades[4]} => Pass");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{dblGrades[4]} => Fail");
+            //}
+
+            //double dblGradesSum = dblGrades[0] + dblGrades[1] + dblGrades[2] + dblGrades[3] + dblGrades[4]  ;
+            //double dblAvg = dblGradesSum / dblGrades.Length;
+            //Console.WriteLine($"The average is {dblAvg}");
+            //if (dblAvg < 0 || dblAvg > 100)
+            //{
+            //    Console.WriteLine("Invalid");
+
+            //}
+            //else if (dblAvg >= 60)
+            //{
+            //    Console.WriteLine("Pass");
+
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("Fail");
+            //}
+
+            double dblMyGrade = 78;
+        }
+    }
+}
