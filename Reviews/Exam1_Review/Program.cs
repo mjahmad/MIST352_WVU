7a3086b1c095d7c8bbaa519e6e62de1d482fc59f
MJ Ahmad <mahmad2@mix.wvu.edu>
2026-02-24 11:13:48 -0500

    added for loops

---
+++ Reviews/Exam1_Review/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exam1_Review
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 9;
+            int z;
+            //Console.WriteLine(x+z);
+            Console.WriteLine(z);
+
+
+
+        }
+    }
+}
