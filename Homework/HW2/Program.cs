d553a5103acc6c7a1a96dfd76a528563a9c52fb0
MJ <mahmad2@mix.wvu.edu>
2026-03-05 01:37:42 -0500

    added HW2 data

---
+++ Homework/HW2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace HW2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] students = File.ReadAllLines("data.txt");
+            if (File.Exists("data.txt"))
+                Console.WriteLine("Data read successfully.");
+
+
+        }
+    }
+}
