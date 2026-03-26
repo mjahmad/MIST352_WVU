5162451ee7bb5b82a542b465dd47c2919f701b53
MJ Ahmad <mahmad2@mix.wvu.edu>
2026-03-26 11:14:49 -0400

    added Methods - Thursday 3/26/26

---
+++ In_Class_Examples/Module 7/Methods_1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Methods_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int intNo1 = 100, intNo2 = 90, intNo3 = 60;
+            string strMyName = "Mohammad Jamil Ahmad";
+            
+           //ShowMenu();
+            
+            Console.WriteLine(CalculateAvg(intNo1, intNo2, intNo3));
+
+            Console.WriteLine($"The letter grade of {intNo3} is {CalculateLetterGrade(intNo3)}");
+
+            Console.WriteLine($"The initials of the name {strMyName} is {GetInitials(strMyName)}");
+
+
+        }
+        /// <summary>
+        /// this simply displays the menu options.
+        /// </summary>
+        public static void ShowMenu()
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(" Option 1: Display info");
+            Console.WriteLine(" Option 2: Accept Data");
+            Console.WriteLine(" Option 3: Calcualte GPA");
+            Console.WriteLine(" Option 4: Exit");
+        }
+
+        /// <summary>
+        /// The method calcaultes the average of three given values
+        /// </summary>
+        /// <param name="No1"> 1ST value</param>
+        /// <param name="No2">2nd value</param>
+        /// <param name="No3"> 3rd value</param>
+        /// <returns> The avrage of No1, No2, and No3</returns>
+        public static int CalculateAvg(int No1, int No2, int No3 )
+        {
+            int intAvg = (No1 + No2 + No3) / 3;
+            return intAvg;
+        
+        }
+
+        public static char CalculateLetterGrade(int intTheGrade)
+        {
+            if (intTheGrade >= 90)
+                return 'A';
+            else if (intTheGrade >= 80)
+                return 'B';
+            else if (intTheGrade >= 70)
+                return 'C';
+            else if (intTheGrade >= 60)
+                return 'D';
+            else
+                return 'F';
+        
+        }
+
+        /// <summary>
+        /// This method accetps a name and return the initials of that name
+        /// </summary>
+        /// <param name="theName"> The name from the main mehtod</param>
+        /// <returns>returns the initials</returns>
+        public static string GetInitials(string theName)
+        {
+            string[] splittedName = theName.Split(" ");
+            string strFirstName = splittedName[0];
+            string strMidName = splittedName[1];
+            string strLastName = splittedName[2];
+            string chr1 = strFirstName.Substring(0,1);
+            string chr2 = strMidName.Substring(0, 1);
+            string chr3 = strLastName.Substring(0, 1);
+            return chr1 + chr2 + chr3;
+
+       }
+
+    }
+}
---
+++ In_Class_Examples/Module 7/Methods_2/Program.cs
@@ -0,0 +1,132 @@
+﻿using System.Reflection.PortableExecutable;
+
+namespace Methods_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int[] intData = { 90, 100, 77, -1, -9, 0,0,0,1,200 };
+
+            //DisplayAll(intData);
+            if (ValidIdentity())
+                Console.WriteLine("Welcome");
+            else
+                Console.WriteLine("Invalid user or password");
+
+            string[] theNames = AcceptNames();
+            PrintDataInArray(theNames);
+
+
+
+        }
+
+        /// <summary>
+        /// This simply prints out all data in any given array
+        /// </summary>
+        /// <param name="names"></param>
+        public static void PrintDataInArray(string[] names)
+        {
+            for (int intIndex = 0; intIndex < names.Length; intIndex++)
+            {
+                Console.WriteLine(names[intIndex]);
+            }
+
+        }
+
+        /// <summary>
+        /// This method accepts 5 names from user into an array
+        /// </summary>
+        /// <returns>The array of names</returns>
+        public static string[] AcceptNames()
+            {
+            string[] strNames = new string[5];
+            for (int intdIndex = 0; intdIndex < strNames.Length; intdIndex++)
+            {
+                Console.WriteLine($"Give me name No. {intdIndex+1}");
+                strNames[intdIndex] = Console.ReadLine(); 
+
+            }
+            return strNames;
+
+
+            }
+
+
+        public static void DisplayAll(int[] intData)
+        {
+            DisplayInfo();
+            Console.WriteLine(FindMax(intData));
+
+            Console.WriteLine(FindMin(intData));
+        }
+
+        /// <summary>
+        /// This msethod verifies the identity of a given user
+        /// It accepts user name and password and verifies they are equal to pre-coded credintials.
+        /// </summary>
+        /// <returns>True if user and pass are correct, false otherwise.<returns>
+        public static bool ValidIdentity()
+        {
+            Console.WriteLine("Give me your user name");
+            string strUser = Console.ReadLine();
+
+            Console.WriteLine("Give me your password");
+            string strPass = Console.ReadLine();
+            if (strUser.Equals("user1") && strPass.Equals("pass1"))
+                return true;
+
+            return false;
+        }
+
+
+        /// <summary>
+        /// This method finds the max value in a given array
+        /// </summary>
+        /// <param name="theData"> The array or integers </param>
+        /// <returns>The maximum value in the array</returns>
+        public static int FindMax(int[] theData)
+        {
+            int intMax = theData[0];
+            for (int intIndex = 0; intIndex < theData.Length; intIndex++)
+            {
+                if (theData[intIndex] >= intMax)
+                    intMax = theData[intIndex];
+
+            }
+            return intMax;
+        
+        }
+
+        /// <summary>
+        /// This method finds the min value in a given array
+        /// </summary>
+        /// <param name="theData"> The array or integers </param>
+        /// <returns>The min value in the array</returns>
+        public static int FindMin(int[] theData)
+        {
+            int intMin = theData[0];
+            for (int intIndex = 0; intIndex < theData.Length; intIndex++)
+            {
+                if (theData[intIndex] <= intMin)
+                    intMin = theData[intIndex];
+
+            }
+            return intMin;
+
+        }
+        /// <summary>
+        /// This simply calls all methods in this program
+        /// It prints out the messages in DisplayInfo, and min and max by calling 
+        /// the two other methods (FindMin , and FindMax).
+        /// </summary>
+        public static void DisplayInfo()
+        {
+            Console.WriteLine("Good morning, World!");
+            Console.WriteLine("This will find min and max!");
+
+        }
+
+    }
+}
