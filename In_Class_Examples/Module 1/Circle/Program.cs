13803a2995904df81cbc3565527b2f9e69c2a1d3
MJ Ahmad <mahmad2@mix.wvu.edu>
2026-09-01 12:50:54 -0400

    added more examples modules 1-4

---
+++ In_Class_Examples/Module 1/Circle/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Circle Program
+ * 
+ */
+
+using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+    {
+        // A = πr², where r is the radius of the circle.
+        Console.WriteLine("Helo, this calcalutes the ara of a cirlce.");
+        Console.WriteLine("What is the raduis?");
+        
+        double dblRaduis = Convert.ToDouble(Console.ReadLine());
+        double dblArea = Math.PI * Math.Pow(dblRaduis, 2);
+
+        Console.WriteLine($"The area of circle of {dblRaduis} raduis is {dblArea}");
+        //Console.WriteLine("The area of circle of " + dblRaduis+ " raduis is " +  dblArea);
+
+    }
+}
+
---
+++ In_Class_Examples/Module 1/Payroll/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Mj Ahmad
+ * Payroll system
+ * 8/27/26
+ */
+using System;   
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        // INPUT
+        Console.WriteLine("Enter your first name");
+        string strFirstName =Console.ReadLine();
+
+        Console.WriteLine("Enter your mid name Initial");
+        char chrMidInitial = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+
+        Console.WriteLine("Enter your last name");
+        string strLastName = Console.ReadLine();
+        
+        
+        Console.WriteLine("Enter price per item:");
+        double pricePerItem = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter quantity:");
+        int quantity = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter tax rate as a decimal (example: 0.06):");
+        double taxRate = double.Parse(Console.ReadLine());
+
+        // PROCESSING
+        double subTotal = pricePerItem * quantity;
+        double taxAmount = subTotal * taxRate;
+        double finalTotal = subTotal + taxAmount;
+
+        // OUTPUT
+        Console.WriteLine();
+        Console.WriteLine("----- INVOICE SUMMARY -----");
+        Console.WriteLine("Subtotal: $" + subTotal);
+        Console.WriteLine("Tax:      $" + taxAmount);
+        Console.WriteLine("Total:    $" + finalTotal);
+        Console.WriteLine($"Good bye {strFirstName} {chrMidInitial}. {strLastName}");
+    }
+    }
+
